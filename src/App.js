8426c7edc0b030a1d9f32fef7fdc20b9b9a38eff
Thomas Gregory <ThomJGregory@gmail.com>
2022-06-23 21:37:48 -0500

    Fully functional front end.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
 
 function App() {
+	const [showAddTask, setShowAddTask] = useState(false)
 	const [tasks, setTasks] = useState([
 		{
 			id: 1,
@@ -37,6 +38,15 @@ function App() {
 		}
 	])
 
+	// Add task
+	const addTask = task => {
+		console.log(task)
+		const id = Math.floor(Math.random() * 10000) + 1
+		console.log(id)
+		const newTask = { id, ...task }
+		setTasks([...tasks, newTask])
+	}
+
 	// Delete task
 	const deleteTask = id => {
 		console.log('delete', id)
@@ -51,10 +61,14 @@ function App() {
 		)
 	}
 
+	// Interesting terniary operator in line 68
 	return (
 		<div className="container">
-			<Header />
-			<AddTask />
+			<Header
+				onAdd={() => setShowAddTask(!showAddTask)}
+				showAdd={showAddTask}
+			/>
+			{showAddTask && <AddTask onAdd={addTask} />}
 			{tasks.length > 0 ? (
 				<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
 			) : (
